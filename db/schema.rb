40988ebf00dc9629441db05dd98da8d22dead519
Svetlana Ivanova <svetlana.zh.ivanova@izpitai.me>
2017-01-25 13:34:21 +0000

    91 create users tabel and model

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161219145644) do
+ActiveRecord::Schema.define(version: 20170125131246) do
 
   create_table "articles", force: :cascade do |t|
     t.text     "description"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "title"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "username"
+    t.string   "email"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
