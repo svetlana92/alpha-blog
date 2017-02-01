381b0d9a0ce4cd66106cb1616ff0a7d9712adc68
Svetlana Ivanova <svetlana.zh.ivanova@izpitai.me>
2017-02-01 13:52:45 +0000

    109 111 113 115 117

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,9 @@
 class UsersController < ApplicationController
     
+    before_action :set_user, only: [:edit, :update, :show]
+    
     def index
-        @users = User.all
+        @users = User.paginate(page: params[:page], per_page: 5)
     end
     
     def new
@@ -19,11 +21,10 @@ class UsersController < ApplicationController
     end
     
     def edit
-        @user = User.find(params[:id])
+
     end
     
     def update
-        @user = User.find(params[:id])
         if @user.update(user_params)
             flash[:success] = "Updated successfully"
             redirect_to articles_path
@@ -33,7 +34,7 @@ class UsersController < ApplicationController
     end
     
     def show
-        @user = User.find(params[:id])
+        @user_articles = @user.articles.paginate(page: params[:page], per_page: 5)
     end
     
     private
@@ -41,4 +42,9 @@ class UsersController < ApplicationController
     def user_params
         params.require(:user).permit(:username, :email, :password)
     end
+    
+    def set_user
+        @user = User.find(params[:id])
+    end
+    
 end
