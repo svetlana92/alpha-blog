8d078572cace4e0698e9071de2ca1329e75967c2
Svetlana Ivanova <svetlana.zh.ivanova@izpitai.me>
2017-02-02 15:31:28 +0000

    121 123 125

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -57,7 +57,7 @@ class ArticlesController < ApplicationController
     end
     
     def require_same_user
-        if current_user != @article.user
+        if current_user != @article.user and !current_user.admin?
             flash[:danger] = 'This is not your article!'
             redirect_to root_path
         end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
     
     before_action :set_user, only: [:edit, :update, :show]
     before_action :require_same_user, only: [:edit, :update, :destroy]
+    before_action :require_admin, only: [:destroy]
     
     def index
         @users = User.paginate(page: params[:page], per_page: 5)
@@ -14,8 +15,9 @@ class UsersController < ApplicationController
     def create
         @user = User.new(user_params)
         if @user.save
+            session[:user_id] = @user.id
             flash[:success] = "Welcome #{@user.username}"
-            redirect_to articles_path
+            redirect_to user_path(@user)
         else
             render 'new'
         end
@@ -38,6 +40,13 @@ class UsersController < ApplicationController
         @user_articles = @user.articles.paginate(page: params[:page], per_page: 5)
     end
     
+    def destroy
+        @user = User.find(params[:id])
+        @user.destroy
+        flash[:danger] = "User and all articles created by user have been deleted"
+        redirect_to users_path
+    end
+    
     private
     
     def user_params
@@ -49,8 +58,15 @@ class UsersController < ApplicationController
     end
     
     def require_same_user
-        if current_user != @user
+        if current_user != @user and !current_user.admin?
             flash[:danger] = 'This is not your account!'
+            redirect_to root_path
+        end
+    end
+    
+    def require_admin
+        if logged_in? and !current_user.admin?
+            flash[:danger] = "Only admin can do that"
             redirect_to root_path
         end
     end
