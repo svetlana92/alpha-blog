7b15b174c55a7884bcfb52e6b1e7e15a39ed811d
Svetlana Ivanova <svetlana.zh.ivanova@izpitai.me>
2017-02-06 16:17:10 +0000

    128 130 132

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170202130203) do
+ActiveRecord::Schema.define(version: 20170206132643) do
 
   create_table "articles", force: :cascade do |t|
     t.text     "description"
@@ -19,6 +19,12 @@ ActiveRecord::Schema.define(version: 20170202130203) do
     t.datetime "updated_at"
     t.string   "title"
     t.integer  "user_id"
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "users", force: :cascade do |t|
