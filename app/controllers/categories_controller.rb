b8e47dc59c5ffa7d2b7fddd67b89c0a9ee585fd6
Svetlana Ivanova <svetlana.zh.ivanova@izpitai.me>
2017-02-08 12:21:27 +0000

    150

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,6 @@
 class CategoriesController < ApplicationController
 
+    before_action :set_category, only: [:edit, :update, :show]
     before_action :require_admin, except: [:index, :show]
     
     def index
@@ -20,12 +21,28 @@ class CategoriesController < ApplicationController
         end
     end
     
+    def edit
+
+    end
+    
+    def update
+        if @category.update(category_params)
+            flash[:success] = "Category name was successfully updated"
+            redirect_to category_path(@category)
+        else
+            render 'edit'
+        end
+    end
+    
     def show
-        @category = Category.find(params[:id])
         @category_articles = @category.articles.paginate(page: params[:page], per_page: 5)
     end
     
     private
+    
+    def set_category
+        @category = Category.find(params[:id])
+    end
     
     def category_params
         params.require(:category).permit(:name)
